4cad8208ebc1a21face69e29d76c44080819e4ed
Pranjal Saxena <pranjal9599@gmail.com>
2019-11-29 18:57:17 +0530

    Redesign

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,120 +4,138 @@ import { FaBell } from "react-icons/fa";
 
 export default () => {
   return (
-    <Centered>
+    <Container>
       <Head>
         <title>WebAssembly</title>
       </Head>
-      <div>
-        <header role="banner">
-          <Headline>
-            <Logo alt="WebAssembly logo" src="/static/webassembly-logo.svg" />
-          </Headline>
-          <Headline>
-            WebAssembly<br></br> Summit
-          </Headline>
-        </header>
-        <GrayDiv role="main">
-          <WhiteSpan>February 10</WhiteSpan>, 2020<br></br>
-          <WhiteSpan>Google</WhiteSpan>, Plymouth St 1625<br></br>
-          <WhiteSpan>Mountain View</WhiteSpan> CA
-        </GrayDiv>
-        <div role="contentinfo">
-          <Spacer>
-            <a href="https://bit.ly/wasm-summit-2020">
-              <Button>
-                <FaBell style={{ margin: "0 20px 0 35px" }} role="img" aria-label="Sign Up Icon"></FaBell>
-                <div>Sign up for updates</div>
-              </Button>
-            </a>
-          </Spacer>
-          <Divider />
-          <ContactUs>
-            Contact us: <a href="mailto:wasm-summit-2020@chromium.org">wasm-summit-2020@chromium.org</a>
-          </ContactUs>
+
+      <Hero>
+        <Logo src="/static/logo.svg" alt="Web assembly summit logo" />
+        <div>
+          <Headline>WebAssembly summit</Headline>
+          <Subheadline>February 10, 2020 <br />Google, Plymouth St 1625<br />Mountain View CA</Subheadline>
         </div>
-      </div>
-    </Centered>
+      </Hero>
+
+      <Content>
+        Qui exercitation irure est aliquip mollit id ipsum proident eu culpa commodo enim nostrud. Aute laboris do sint esse mollit magn.
+      </Content>
+
+      <CTA>
+        <Button primary href="#">Apply as Speaker</Button>
+        <Button primary={false} href="#">Apply for tickets</Button>
+      </CTA>
+
+      <Footer>
+        <a href="#">Contact</a>
+        |
+        <a href="#">Code of conduct</a>
+      </Footer>
+
+    </Container>
   );
 };
 
 const Logo = styled.img`
-  max-width: 450px;
-  width: 200px;
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: 20px;
+
+  @media(max-width: 768px) {
+    margin-bottom: 40px;
+  }
 `;
 
-const Centered = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+const Container = styled.div`
+  max-width: 1440px;
+  padding: 0px 100px;
+  margin: 0 auto;
+  display:flex;
+  height: 100vh;
   align-items: center;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  justify-content: center;
+  flex-direction: column;
+  @media(max-width: 768px) {
+    padding: 10px 20px;
+  }
 `;
 
-const WhiteSpan = styled.span`
-  font-size: 20px;
-  color: white;
-`;
-
-const GrayDiv = styled.div`
-  font-size: 20px;
-  color: rgba(255, 255, 255, 0.7);
-  margin: 20px 20px;
-  line-height: 1.5;
-`;
+const Hero = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: flex-end;
+  aling-content: center;
+  @media(max-width: 1020px) {
+    grid-template-columns: 1fr;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+`
 
 const Headline = styled.h1`
-  font-size: 38px;
-  color: white;
-  letter-spacing: 7px;
-  margin: 20px;
-`;
-
-const Button = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: stretch;
-  font-size: 20px;
-  color: rgba(255, 255, 255, 0.75);
-  border: 1px solid rgba(255, 255, 255, 0.5);
-  background-color: rgba(255, 255, 255, 0.05);
-  border-radius: 25px;
-  padding: 10px 5px;
-  font-weight: bold;
-  text-align: center;
-  transition: 150ms;
-
-  &:hover {
-    border: 1px solid transparent;
-    box-shadow: 8px 8px 5px rgba(0, 0, 0, 0.05);
-    transform: scale(1.04);
-    color: black;
-    background-color: rgba(255, 255, 0, 0.75);
+  font-size: 4rem;
+  text-shadow: 1px 4px 0px rgba(0, 0, 0, 0.1);
+  margin: 20px 0px;
+  padding: 0px;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 400;
+  @media(max-width: 768px) {
+    font-size: 3rem;
   }
-`;
+`
 
-const Spacer = styled.div`
-  font-size: 20px;
-  line-height: 1.5;
-  padding: 10px 20px 0 20px;
-`;
+const Subheadline = styled.h3`
+  font-size: 1.78rem;
+  font-weight: 500;
+  color: rgba(255,255,255,0.8);
+  margin: 0; padding: 0;
+`
 
-const Divider = styled.hr`
-  border: 1px solid rgba(255, 255, 255, 0.8);
-  margin-top: 3em;
-  margin-bottom: 1em;
-`;
-
-const ContactUs = styled.div`
-  color: #FFFFFF;
+const Content = styled.p`
+  font-size: 1.4rem;
+  color: rgba(255,255,255,0.7);
   text-align: center;
+  max-width: 900px;
+  margin: 40px auto;
+`
 
-  a, a:visited {
-    color: rgba(255, 255, 255, 0.7);
+const CTA = styled.div`
+  margin: 0 auto;
+  text-align: center
+`
+
+const Button = styled.a`
+  font-size: 1.67rem;
+  padding: 25px 40px;
+  background: ${(props: { primary: boolean }) => props.primary ? '#2D16A4' : '#fff'};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
+  margin: 0px 10px;
+  color: ${(props: { primary: boolean }) => props.primary ? '#fff' : '#2D16A4'};
+  transition: all 0.2s ease;
+  display: inline-block;
+  &:hover { 
+    box-shadow: 0px 4px 20px rgba(0,0,0,0.2);
+    transform: scale(1.07);
   }
-`;
 
+  @media (max-width: 768px) {
+    padding: 10px 20px;
+    margin: 5px 5px;
+  }
+`
+
+const Footer = styled.footer`
+  text-align: center;
+  color: rgba(255,255,255,0.7);
+  margin: 40px 0px;
+  font-size: 1.1rem;
+  a {
+    color: rgba(255,255,255,0.7);
+    margin: 0px 10px;
+    &:hover {
+      color: rgba(255,255,255,1);
+    }
+  }
+`
