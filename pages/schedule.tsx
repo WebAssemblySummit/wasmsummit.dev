d56d680129323f2270c039c586a2615fed15d267
Aaron Turner <aaron@aaronthedev.com>
2020-01-15 19:37:59 -0800

    Made the basic schedule page design

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -4,12 +4,71 @@ import { NavBar, navbarBlue } from "../components";
 
 const title = "Schedule";
 
+type mockScheduleItem = {
+  time: string,
+  title: string,
+  description?: string
+  link?: string
+}
+
+const scheduleItems: mockScheduleItem[] = [
+  {
+    time: "8:00am",
+    title: "breakfast"
+  },
+  {
+    time: "9:15am",
+    title: "Introductions"
+  },
+  {
+    time: "9:30am",
+    title: "Lin Clark",
+    description: "Opening Keynote",
+    link: "/speakers/1/"
+  },
+  {
+    time: "10:15am",
+    title: "Alon Zakai",
+    description: "Shipping Tiny WebAssembly Builds",
+    link: "/speakers/2/"
+  }
+
+];
+
 const SchedulePage: FC = () => (
   <>
   <Background />
   <NavBar title={title} backgroundColor={navbarBlue} bottom />
   <Container>
     <Headline>{title}</Headline>
+    <Schedule>
+      {scheduleItems.map(item => {
+
+        let titleAndDescription = (
+          <>
+          <ScheduleItemTitle>{item.title}</ScheduleItemTitle>
+          <ScheduleItemDescription>{item.description}</ScheduleItemDescription>
+          </>
+        );
+
+        if (item.link) {
+          titleAndDescription = (
+            <ScheduleLink href={item.link}>
+              {titleAndDescription}
+            </ScheduleLink>
+          )
+        }
+
+        let itemJsx = (
+          <ScheduleItem>
+            <ScheduleItemTime>{item.time}</ScheduleItemTime>
+            {titleAndDescription}
+          </ScheduleItem>
+        );
+
+        return itemJsx;
+      })}
+    </Schedule>
   </Container>
   </>
 );
@@ -30,12 +89,45 @@ export const Headline = styled.h1`
   font-size: 3em;
   margin-top: 3vh;
   margin-bottom: 0vh;
-  padding: 10px;
 `;
 
 export const Container = styled.div`
   padding: 3vh 9vw;
   color: white;
 `;
+
+export const Schedule = styled.ul`
+  list-style-type: none;
+  padding: 0px;
+  padding-left: 0px;
+`;
+
+export const ScheduleItem = styled.li`
+  border-left: 2px solid #FFFFFF;
+
+  padding-left: 10px;
+  margin-bottom: 10px;
+`;
+
+export const ScheduleItemTitle = styled.h2`
+  margin-bottom: 5px;
+`;
+
+export const ScheduleItemTime = styled.p`
+
+`;
+
+export const ScheduleItemDescription = styled.p`
+  margin-top: 0px;
+`;
+
+export const ScheduleLink = styled.a`
+  color: #FFFFFF;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
 
 
