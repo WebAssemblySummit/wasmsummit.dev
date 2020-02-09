d081ffcdcdd7834740180248167ee7a5e99cae4c
Aaron Turner <aaron@aaronthedev.com>
2020-02-09 14:25:40 -0800

    Added the youtube Livestream link

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,11 +22,21 @@ const LandingPage: FC = () => (
           </Subheadline>
         </Title>
       </Hero>
+
       <Oneliner>
         A <OnelinerHighlight>one day, single track</OnelinerHighlight>,
         conference about <OnelinerHighlight>all things</OnelinerHighlight>{" "}
         WebAssembly.
       </Oneliner>
+
+      <CallToAction>
+        <Button
+          primary
+          href="https://youtu.be/WZp0sPDvWfw"
+        >
+          Livestream
+        </Button>
+      </CallToAction>
     </Container>
   </View>
 );
@@ -162,6 +172,10 @@ const Oneliner = styled.div`
     font-size: 1rem;
   }
 
+  @media (max-width: 350px) {
+    display: none;
+  }
+
   @media (max-width: 1024px) and (orientation: portrait) {
     margin-top: 0;
   }
@@ -183,3 +197,31 @@ const OnelinerHighlight = styled.span`
     font-size: 1.1rem;
   }
 `;
+
+const CallToAction = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`;
+
+    const Button = styled.a`
+  font-size: 1.6rem;
+  padding: 15px 20px;
+  background: ${(props: { primary?: boolean }) =>
+    props.primary ? "#2D16A4" : "#fff"};
+  box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  margin: 4vh 20px;
+  color: ${(props: { primary?: boolean }) =>
+    props.primary ? "#fff" : "#2D16A4"};
+  transition: all 0.1s ease;
+  display: inline-block;
+  &:hover {
+    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
+    transform: scale(1.07);
+  }
+  @media (max-width: 768px) {
+    padding: 10px 20px;
+    margin: 10px 5px;
+  }
+`;
+
