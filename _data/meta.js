15ea4e13440592c0c1d1aefbd2c965b42ac5d6b1
Surma <surma@surma.dev>
2020-11-10 14:29:54 +0000

    Very simple design lol

--- _data/meta.js
+++ _data/meta.js
@@ -1,9 +1,17 @@
 module.exports = {
   site_name: "WebAssembly Summit",
   year: 2021,
+  colors: {
+    blue: "#3138c4",
+    "dark-blue": "#1b1c6f",
+    offwhite: "#cbccee",
+    white: "#fff",
+    purple: "#876AD1",
+  },
   canonical_url: "https://webassembly-summit.org",
   twitter: "https://twitter.com/@WasmSummit",
   discord: "https://discord.gg/mQTpt9nS",
   youtube: "https://youtube.com/c/WebAssemblySummit",
   github: "https://github.com/WebAssemblySummit/webassembly-summit.org",
+  cfp: "#",
 };
---
+++ src/faq.md
@@ -0,0 +1,12 @@
+---
+layout: default.ejs
+permalink: faq.html
+---
+
+# Question 1
+
+Answer 1
+
+# Question 2
+
+Answer 2
