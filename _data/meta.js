79e9ea74a1b128412b37c2eefc5a1da89104958e
Surma <surma@surma.dev>
2021-03-17 12:32:30 +0000

    Automate schedule

--- _data/meta.js
+++ _data/meta.js
@@ -8,6 +8,7 @@ module.exports = {
     white: "#fff",
     purple: "#876AD1",
   },
+  start: new Date("2021-04-22 11:30:00 GMT+0100"),
   canonical_url: "https://webassembly-summit.org",
   twitter: "https://twitter.com/wasm_summit",
   discord: "https://discord.gg/cTVXmDfSYR",
